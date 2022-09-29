ec0cab6c49897aa5c14ff94457ad6e89f7d1b4c1
Robert <rjsf06@gmail.com>
2022-09-29 17:12:13 -0300

    :sparkles: feat: localStorage working

--- assets/scripts/main/main.js
+++ assets/scripts/main/main.js
@@ -13,10 +13,11 @@ const calendarSaturday = document.querySelector(".calendar__saturday")
 const calendarSunday = document.querySelector(".calendar__sunday")
 const deleteDiv = document.querySelector(".calendar__remove")
 const deleteAll = document.querySelector(".add__remove")
-const item = document.querySelector(".calendar__item")
+
+const items = JSON.parse(localStorage.getItem('calendar_itens')) || []
 
 add.addEventListener("click", () => {
-    if (content.value.length > 5 && hour.value.length > 2){
+    if (content.value.length > 2 && hour.value.length > 2){
         addCalendar()
     }
     cleanInput()
@@ -27,221 +28,143 @@ function cleanInput() {
     week.value = "Segunda-Feira"
     hour.value = ""
 }
-function addCalendar(){
-    calendar.classList.add("calendar")
-    calendar.classList.remove("disable")
-    var name = content.value
-    var weekdays = week.value
-    var time = hour.value   
 
-    if(weekdays === "Segunda-Feira"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarMonday.appendChild(div)
+function showItems() {
+    calendarMonday.innerHTML = ''
+    calendarTuesday.innerHTML = ''
+    calendarThursday.innerHTML = ''
+    calendarWednesday.innerHTML = ''
+    calendarFriday.innerHTML = ''
+    calendarSaturday.innerHTML = ''
+    calendarSunday.innerHTML = ''
+    items.sort(order)
+    items.forEach((item) => {
 
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--monday")   
-        div.appendChild(divHour)
+        var div = ""
+        var divHour = ""
+        var h4Time = ""
+        var divContent = ""
+        var status = ""
 
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
-
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
-
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--monday")
-        divContent.appendChild(status)
-         
-    } else if(weekdays === "Terça-Feira"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarTuesday.appendChild(div)
-
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--Tuesday")   
-        div.appendChild(divHour)
+        if(item.weekdays === "Segunda-Feira"){
+            createDivCalendar(calendarMonday)
+            createDivHour("calendar__hour--monday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--monday")
+        } else if(item.weekdays === "Terça-Feira"){
+            createDivCalendar(calendarTuesday)
+            createDivHour("calendar__hour--Tuesday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--tuesday")
+        }else if(item.weekdays === "Quarta-Feira"){
+            createDivCalendar(calendarThursday)
+            createDivHour("calendar__hour--Thursday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--thursday")
+        }else if(item.weekdays === "Quinta-Feira"){
+            createDivCalendar(calendarWednesday)
+            createDivHour("calendar__hour--Wednesday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--wednesday")
+        }else if(item.weekdays === "Sexta-Feria"){
+            createDivCalendar(calendarFriday)
+            createDivHour("calendar__hour--Friday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--friday")
+        }else if(item.weekdays === "Sabado"){
+            createDivCalendar(calendarSaturday)
+            createDivHour("calendar__hour--Saturday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--saturday")
+        }else if(item.weekdays === "Domingo"){
+            createDivCalendar(calendarSunday)
+            createDivHour("calendar__hour--Sunday")
+            createH4Time()
+            createDivContent()
+            createDivStatus("calendar__status--sunday")
+        }
+        function createDivCalendar(mes){
+            div = document.createElement("div");
+            div.classList.add("calendar__item")
+            mes.appendChild(div)
+        }
         
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
+        function createDivHour(hour) {
+            divHour = document.createElement("div")
+            divHour.classList.add("calendar__hour")
+            divHour.classList.add(hour)   
+            div.appendChild(divHour)
+        }
 
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
+        function createH4Time(){
+            h4Time = document.createElement("h4")
+            h4Time.classList.add("calendar__time")
+            divHour.appendChild(h4Time)
+            h4Time.textContent = item.time
+        }
+        function createDivContent(){
+            divContent = document.createElement("div")
+            divContent.classList.add("calendar__content")
+            div.appendChild(divContent)
+        }
 
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--tuesday")
-        divContent.appendChild(status)
-    }else if(weekdays === "Quarta-Feira"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarThursday.appendChild(div)
+        function createDivStatus(statusName){
+            status = document.createElement("div")
+            status.classList.add("calendar__status")
+            status.classList.add(statusName)
+            divContent.appendChild(status)
+        }
 
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--Thursday")   
-        div.appendChild(divHour)
+        var h3Text = document.createElement("h3")
+        h3Text.classList.add("calendar__text")
+        divContent.appendChild(h3Text)
+        h3Text.textContent = item.name   
+        
+        var btn = document.createElement("button")
+        btn.classList.add("calendar__remove")
+        divContent.appendChild(btn)
+        btn.innerHTML = "Apagar"
+        btn.setAttribute("onclick", `deleteOne(${items.indexOf(item)})`)
+    })
+}
 
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
-
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
-
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--thursday")
-        divContent.appendChild(status)
-    }else if(weekdays === "Quinta-Feira"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarWednesday.appendChild(div)
-
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--Wednesday")   
-        div.appendChild(divHour)
-
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
-
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
-
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--wednesday")
-        divContent.appendChild(status)
-    }else if(weekdays === "Sexta-Feria"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarFriday.appendChild(div)
-
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--Friday")   
-        div.appendChild(divHour)
-
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
-
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
-
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--friday")
-        divContent.appendChild(status)
-    }else if(weekdays === "Sabado"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarSaturday.appendChild(div)
-
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--Saturday")   
-        div.appendChild(divHour)
-
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
-
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
-
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--saturday")
-        divContent.appendChild(status)
-    }else if(weekdays === "Domingo"){
-        var div = document.createElement("div");
-        div.classList.add("calendar__item")
-        calendarSunday.appendChild(div)
-
-        var divHour = document.createElement("div")
-        divHour.classList.add("calendar__hour")
-        divHour.classList.add("calendar__hour--Sunday")   
-        div.appendChild(divHour)
-
-        var h4Time = document.createElement("h4")
-        h4Time.classList.add("calendar__time")
-        divHour.appendChild(h4Time)
-        h4Time.textContent = time
-
-        var divContent = document.createElement("div")
-        divContent.classList.add("calendar__content")
-        div.appendChild(divContent)
-
-        var status = document.createElement("div")
-        status.classList.add("calendar__status")
-        status.classList.add("calendar__status--sunday")
-        divContent.appendChild(status)
+function addCalendar(){
+    const item = {
+        name: content.value,
+        weekdays: week.value,
+        time: hour.value, 
     }
-    var h3Text = document.createElement("h3")
-    h3Text.classList.add("calendar__text")
-    divContent.appendChild(h3Text)
-    h3Text.textContent = name   
-    
-    var btn = document.createElement("button")
-    btn.classList.add("calendar__remove")
-    divContent.appendChild(btn)
-    btn.innerHTML = "Apagar"
-    btn.setAttribute("onclick", "deleteOne(this)")
+    items.push(item)
+    showItems()
+}
+
+function deleteOne(index) {
+    items.splice(index, 1)
+    console.log(items)
+    showItems()
+}
+
+function saveFromLocalStorage() {
+    localStorage.setItem('calendar_itens', JSON.stringify(items))
+}
+
+function deleteFromLocalStorage() {
+    localStorage.removeItem('calendar_itens')
 }
 
 deleteAll.addEventListener("click", () => {
-    if(calendarMonday.className === 'calendar__monday'){
-        while (calendarMonday.firstChild) {
-            calendarMonday.removeChild(calendarMonday.firstChild);
-        }
-    } else if (calendarTuesday.className === 'calendar__tuesday'){
-        while (calendarTuesday.firstChild) {
-            calendarTuesday.removeChild(calendarTuesday.firstChild);
-        }
-    }else if (calendarThursday.className === 'calendar__thursday'){
-        while (calendarThursday.firstChild) {
-            calendarThursday.removeChild(calendarThursday.firstChild);
-        }
-    }else if (calendarWednesday.className === 'calendar__wednesday'){
-        while (calendarWednesday.firstChild) {
-            calendarWednesday.removeChild(calendarWednesday.firstChild);
-        }
-    }else if (calendarFriday.className === 'calendar__friday'){
-        while (calendarFriday.firstChild) {
-            calendarFriday.removeChild(calendarFriday.firstChild);
-        }
-    }else if (calendarSaturday.className === 'calendar__saturday'){
-        while (calendarSaturday.firstChild) {
-            calendarSaturday.removeChild(calendarSaturday.firstChild);
-        }
-    }else if (calendarSunday.className === 'calendar__sunday'){
-        while (calendarSunday.firstChild) {
-            calendarSunday.removeChild(calendarSunday.firstChild);
-        }
-    }      
+    items.splice(0, items.length);
+    showItems()
 })
 
-function deleteOne(button){
-    var calendarDiv = button.parentNode.parentNode
-    calendarDiv.parentNode.removeChild(calendarDiv)
+function order(a,b) {
+    return a.time.replace(':', '.') - b.time.replace(':', '.')
 }
+
+showItems()
