7df719e4f66a643bd5b00246dcbec4d83979e30d
Robert <rjsf06@gmail.com>
2022-09-29 07:09:35 -0300

    :construction: doing the localstorage

---
+++ assets/scripts/localstorage.js
@@ -0,0 +1,22 @@
+// const text = document.querySelector(".calendar__time")
+// const btnSalvar = document.querySelector(".buttons__save")
+// const btnRemove = document.querySelector(".buttons__remove")
+
+// const itens = []
+
+// btnSalvar.addEventListener("click", () =>{
+
+//     const itensAtuais = {
+//         nome: text.value
+//     }
+
+//     itens.push(itensAtuais)
+//     localStorage.setItem("text", JSON.stringify(itens))
+    
+// })
+
+
+
+// btnRemove.addEventListener("click", () =>{
+//     localStorage.clear()
+// })
